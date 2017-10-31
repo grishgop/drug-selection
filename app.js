afc7ff04790c53527966d9b023d5a8a93b4f3b76
grishgop <33265387+grishgop@users.noreply.github.com>
2017-10-31 17:59:00 -0400

    update cred

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(bodyParser.json());
 var port = (process.env.VCAP_APP_PORT || process.env.PORT || 3000);
 var host = (process.env.VCAP_APP_HOST || process.env.HOST || 'localhost');
 
-var defaultBaseURL = '<from your service instance on Bluemix>';
-var defaultAccessKey = '<from your service instance on Bluemix>';
+var defaultBaseURL = 'https://ibm-watson-ml.mybluemix.net';
+var defaultAccessKey = '6kj4CqTfzgZZGS3jrVnWxuyuqypoAR7gfjj9opvrYgusKACpBQBcrvJrZguSAabeHxGxQ3pIogjgEOjN0TGDTcL0h32gVzPkwMbmHXNpi+FQYUqQmv73SQJrb1WXWeZv';
 
 // VCAP_SERVICES contains all the credentials of services bound to
 // this application. For details of its content, please refer to
